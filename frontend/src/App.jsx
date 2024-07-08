110594ed07eeb733bbfc3ade1fb2f06f8a13adc8
AbhinavMangalore16 <101643788+AbhinavMangalore16@users.noreply.github.com>
2024-07-09 00:17:16 +0530

    Frontend near comp.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { Transfer } from "./pages/Transfer";
 
+function App() {
   return (
-    <div>
-        Welcome to VeloPay!
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transfer" element={<Transfer/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
